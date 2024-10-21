063c96ac2dd611f7394f8cfbfeeacb9a368d7a54
jotavee-dev <joaovitorengcomp@edu.unifor.br>
2024-10-21 17:13:08 -0300

    primeiro commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+// Componente Header
+function Header() {
+  return (
+    <>
+      <header>BANNER HEADER</header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header/header';
+import Nav from '../../components/Nav/Nav';
+
+function Home() {
+    return(
+        <>
+        <Header></Header>
+        <Nav></Nav>
+        </>
+    )
+}
+
+export default Home;
